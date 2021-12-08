2df1acd8cfddf13e5792be078d5a399496f740d6
Jamie Hoover <uipoet@users.noreply.github.com>
2021-12-08 09:07:47 -0800

    chore: website font fix

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -1,4 +1,8 @@
+import "@fontsource/roboto/latin-400.css";
+import "@fontsource/roboto/latin-500.css";
+import "ninjakit/style.css";
 import "ninjakit/color-scheme";
+import "./style.css";
 
 const { hash, pathname, search } = location;
 
