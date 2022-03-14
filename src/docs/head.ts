82653b7c26623dfc80c13d3e106d824dbbd4f9a9
Jamie Hoover <uipoet@users.noreply.github.com>
2022-03-13 17:55:38 -0700

    fix: colorScheme (#245)
    - export `colorScheme` as function instead of self-invoking module
    - update `@floating-ui/react-dom` and usage accordingly

--- src/docs/head.ts
+++ src/docs/head.ts
@@ -1,5 +1,8 @@
 import "@fontsource/roboto/latin-400.css";
 import "@fontsource/roboto/latin-500.css";
 import "ninjakit/color-scheme/default.css";
-import "ninjakit/color-scheme";
 import "./style.css";
+
+import { colorScheme } from "ninjakit";
+
+colorScheme();
